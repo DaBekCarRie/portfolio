781ae53bbb0464e28402d96807610a7915cc5cc9
Chaini Mitrsatit <joeds133@hotmail.com>
2022-11-03 18:32:46 +0700

    first

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+import { Link } from 'react-scroll'
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi,my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Chaini Mitrsatit</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio culpa sint dolorum, nemo consequuntur error! Quam ullam, magnam iste quaerat dolorem rem dolorum voluptas illo ipsa fuga laboriosam asperiores consequatur, quos vel. Eligendi, libero perferendis? Ducimus neque accusamus iste recusandae?</p>
+            <div>
+                <Link  to="work"  smooth={true}  duration={500} >
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                view work 
+                <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-4'/>
+                </span>
+                </button>
+        </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Skills.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import html from '../assets/html.png'
+import react from '../assets/react.png'
+import tailwind from '../assets/tailwind.png'
+import python from '../assets/python.png'
+import c from '../assets/c.png'
+import javascript from '../assets/javascript.png'
+import css from '../assets/css.png'
+import bootstrap from '../assets/bootstrap.png'
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        {/* containner */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>
+                    Skills
+                </p>
+                <p className='py-4'>
+                    These are some skills my skills
+                </p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 uppercase'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={html} alt="html icon" />
+                    <p className='my-4'>html</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={css} alt="css icon" />
+                    <p className='my-4'>css</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={react} alt="react icon" />
+                    <p className='my-4'>react</p>
+                </div>
+                
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={javascript} alt="javascript icon" />
+                    <p className='my-4'>javascript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto pt-[24px]' src={tailwind} alt="tailwind icon" />
+                    <p className='my-6'>tailwind</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={bootstrap} alt="bootstrap icon" />
+                    <p className='my-4'>bootstrap</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={python} alt="python icon" />
+                    <p className='my-4'>python</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={c} alt="c icon" />
+                    <p className='my-4'>c</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Skills
