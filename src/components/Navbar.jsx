751d4dfa2cf2b77f934e6a66c8f4863528f5da05
DaBekCarRie <98639247+DaBekCarRie@users.noreply.github.com>
2025-06-08 23:02:39 +0700

    Add new work

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,115 +1,156 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes ,FaGithub,FaLinkedin } from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
-import { Link } from 'react-scroll'
-
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [nav,setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
-    <div className='fixed w-full h-[80px] flex justify-end items-center px-4 bg-[#0a192f] text-gray-300'>
-    
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-[#0a192f] text-gray-300 z-50">
       {/* menu จอปกติ */}
-      <div className='hidden md:flex'>
-      <ul className='md:flex'>
-        <li>
-          <Link  to="home"  smooth={true}  duration={500} >
-          Home
-        </Link>
-        </li>
-        <li>
-          <Link  to="about"  smooth={true}  duration={500} >
-          About
-        </Link>
-        </li>
-        <li>
-          <Link  to="skills"  smooth={true}  duration={500} offset={-10}>
-          Skills
-        </Link>
-        </li>
-        <li>
-          <Link  to="work"  smooth={true}  duration={500} offset={-10}>
-          Experience
-        </Link>
-        </li>
-        <li>
-          <Link  to="activity"  smooth={true}  duration={500} offset={-100}>
-          Activity
-        </Link>
-        </li>
-        <li>
-          <Link  to="contact"  smooth={true}  duration={500} >
-          Contact
-        </Link>
-        </li>
-      </ul>
+      <div className="hidden md:flex ">
+        <ul className="md:flex">
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500} offset={-80}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="work" smooth={true} duration={500} offset={-80}>
+              Experience
+            </Link>
+          </li>
+          <li>
+            <Link to="activity" smooth={true} duration={500} offset={-80}>
+              Activity
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
 
       {/* mobile menu จอกลางขึ้นไป จะไม่ขึ้น */}
-      <div onClick={handleClick} className='md:hidden z-10'>
-        {!nav ? <FaBars className='cursor-pointer'/> : <FaTimes/>}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars className="cursor-pointer" /> : <FaTimes />}
       </div>
       {/* mobile menu */}
-      <ul className={!nav ? 'hidden':'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="home"  smooth={true}  duration={500} >
-          Home
-        </Link>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
         </li>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="about"  smooth={true}  duration={500} >
-          About
-        </Link>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
         </li>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="skills"  smooth={true}  duration={500} >
-          Skills
-        </Link>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="skills"
+            smooth={true}
+            duration={500}
+            offset={-80}
+          >
+            Skills
+          </Link>
         </li>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="work"  smooth={true}  duration={500} >
-          Experience
-        </Link>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="work"
+            smooth={true}
+            duration={500}
+            offset={-80}
+          >
+            Experience
+          </Link>
         </li>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="activity"  smooth={true}  duration={500} >
-          Activity
-        </Link>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="activity"
+            smooth={true}
+            duration={500}
+            offset={-80}
+          >
+            Activity
+          </Link>
         </li>
-        <li className='py-6 text-4xl'>
-        <Link onClick={handleClick} to="contact"  smooth={true}  duration={500} >
-          Contact
-        </Link>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
         </li>
-
       </ul>
 
-
       {/* social icons */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-tr-lg'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-            href='https://www.linkedin.com/in/chaini-mitrsatit-9aa99b254/' target='blank'>LinkedIn <FaLinkedin size={30}/></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-tr-lg">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/chaini-mitrsatit-9aa99b254/"
+              target="blank"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2e2c2c]'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-            href='https://github.com/DaBekCarRie' target='blank'>Github <FaGithub size={30}/></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#2e2c2c]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/DaBekCarRie"
+              target="blank"
+            >
+              Github <FaGithub size={30} />
+            </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#073554]'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-            href='mailto:chaini.mst@gmail.com' target='blank'>Email <HiOutlineMail size={30}/></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#073554]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:chaini.mst@gmail.com"
+              target="blank"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#245a1f] rounded-br-lg'>
-            <a className='flex justify-between items-center w-full text-gray-300'
-            href="/RESUME_CHAINI_MITRSATIT.pdf" download target='blank'>Resume <BsFillPersonLinesFill size={30}/></a>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#245a1f] rounded-br-lg">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/RESUME_CHAINI_MITRSATIT.pdf"
+              download
+              target="blank"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
           </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,48 +1,61 @@
-import React from 'react'
-import { works } from '../data/works' 
-import kbtg from '../assets/workpic/kbtg.JPG'
+import React from "react";
+import { works } from "../data/works";
+import kbtg from "../assets/workpic/kbtg.JPG";
 const Work = () => {
-    const worksList = works;
+  const worksList = works;
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pt-8'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
-                <p className='py-6'>check out my term project and best practice projects</p>
-            </div>
-{/* container */}
-            <div className='grid sm:grid-cols-2 gap-4'>{/* md:grid-cols-3  */}
-                {/* grid item */}
-                {/* 1 */}
-                {worksList.map((work,index) => (
-                    <div key={index} style={{backgroundImage: `url(${work.image})`}}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-    
-                        {/* hover */}
-                        <div className='opacity-0 group-hover:opacity-100 text-center'>
-                            <span className='text-2xl font-bold text-white tracking-wider '> 
-                            {work.name}
-    
-                            </span>
-                            <div className='pt-8 text-center'>
-                                <a href={work.demo} target="blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
-                                </a>
-                                <a href={work.github} target="blank">
-                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-                <div  style={{backgroundImage: `url(${kbtg})`}}
-                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    </div>
-            </div>
+    <div
+      name="work"
+      className="w-full md:min-h-screen text-gray-300 bg-[#0a192f] mb-20"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Experience
+          </p>
+          <p className="py-6">
+            check out my term project and best practice projects
+          </p>
         </div>
-
+        {/* container */}
+        <div className="grid sm:grid-cols-2 gap-4">
+          {/* md:grid-cols-3  */}
+          {/* grid item */}
+          {/* 1 */}
+          {worksList.map((work, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${work.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* hover */}
+              <div className="opacity-0 group-hover:opacity-100 text-center">
+                <span className="text-2xl font-bold text-white tracking-wider ">
+                  {work.name}
+                </span>
+                <div className="pt-8 text-center">
+                  <a href={work.demo} target="blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href={work.github} target="blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div
+            style={{ backgroundImage: `url(${kbtg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          ></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
