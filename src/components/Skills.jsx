d4922426446289293465be913afa4e546ba63035
DaBekCarRie <98639247+DaBekCarRie@users.noreply.github.com>
2025-06-10 22:40:20 +0700

    change skill item to re useable

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,5 @@
-import html from "../assets/skillpic/html.png";
-import react from "../assets/skillpic/react.png";
-import tailwind from "../assets/skillpic/tailwind.png";
-import python from "../assets/skillpic/python.png";
-import c from "../assets/skillpic/c.png";
-import javascript from "../assets/skillpic/javascript.png";
-import firebase from "../assets/skillpic/firebase.png";
-import github from "../assets/skillpic/github.webp";
-import nextjs from "../assets/skillpic/nextjs.png";
-import typescript from "../assets/skillpic/typescript.png";
-import css from "../assets/skillpic/css.png";
-import bootstrap from "../assets/skillpic/bootstrap.png";
+import { cn } from "../lib/util";
+import { skills } from "../data/skill";
 const Skills = () => {
   return (
     <div
@@ -26,95 +16,19 @@ const Skills = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 uppercase ">
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={html}
-              alt="html icon"
-            />
-            <p className="my-4">html</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img className="w-20 mx-auto pt-[20px]" src={css} alt="css icon" />
-            <p className="my-4">css</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={react}
-              alt="react icon"
-            />
-            <p className="my-4">reactjs</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={nextjs}
-              alt="nextjs icon"
-            />
-            <p className="my-4">nextjs</p>
-          </div>
-
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={javascript}
-              alt="javascript icon"
-            />
-            <p className="my-4">javascript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={typescript}
-              alt="typescript icon"
-            />
-            <p className="my-4">typescript</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[40px]"
-              src={tailwind}
-              alt="tailwind icon"
-            />
-            <p className="my-6">tailwind</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={bootstrap}
-              alt="bootstrap icon"
-            />
-            <p className="my-4">bootstrap</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={python}
-              alt="python icon"
-            />
-            <p className="my-4">python</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img className="w-20 mx-auto pt-[20px]" src={c} alt="c icon" />
-            <p className="my-4">c</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={github}
-              alt="github icon"
-            />
-            <p className="my-4">github</p>
-          </div>
-          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
-            <img
-              className="w-20 mx-auto pt-[20px]"
-              src={firebase}
-              alt="firebase icon"
-            />
-            <p className="my-4">firebase</p>
-          </div>
+          {skills.map((skill) => (
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-400">
+              <img
+                className={cn(
+                  "w-20 mx-auto ",
+                  skill.css ? skill.css : "pt-[20px]"
+                )}
+                src={skill.img}
+                alt={`${skill.name} icon`}
+              />
+              <p className="my-4">{skill.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/data/skill.js
@@ -0,0 +1,27 @@
+import html from "../assets/skillpic/html.png";
+import react from "../assets/skillpic/react.png";
+import tailwind from "../assets/skillpic/tailwind.png";
+import python from "../assets/skillpic/python.png";
+import c from "../assets/skillpic/c.png";
+import javascript from "../assets/skillpic/javascript.png";
+import firebase from "../assets/skillpic/firebase.png";
+import github from "../assets/skillpic/github.webp";
+import nextjs from "../assets/skillpic/nextjs.png";
+import typescript from "../assets/skillpic/typescript.png";
+import css from "../assets/skillpic/css.png";
+import bootstrap from "../assets/skillpic/bootstrap.png";
+
+export const skills = [
+  { name: "css", img: css },
+  { name: "html", img: html },
+  { name: "react", img: react },
+  { name: "nextjs", img: nextjs },
+  { name: "javascript", img: javascript },
+  { name: "typescript", img: typescript },
+  { name: "bootstrap", img: bootstrap },
+  { name: "tailwind", img: tailwind ,css:'pt-[40px] pb-[10px]'},
+  { name: "python", img: python },
+  { name: "c", img: c },
+  { name: "firebase", img: firebase },
+  { name: "github", img: github },
+];
