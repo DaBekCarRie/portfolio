23cc03f3f2e5f5f29176ccd938291abe7fe9255f
DaBekCarRie <chaini.mst@gmail.com>
2026-06-19 22:08:25 +0700

    fix: about section navigation target conflict

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,10 +26,10 @@ const About = () => {
 
   },[])
   return (
-    <div name="about" className="w-full h-screen text-gray-300">
+    <div id="about" name="about" className="w-full h-screen text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full  grid grid-cols-2 gap-8">
-          <div id="about" className="sm:text-right pb-8 pl-4">
+          <div id="about-title" className="sm:text-right pb-8 pl-4">
             <p className="text-4xl font-bold inline border-b-4 border-pink-600">
               About
             </p>
